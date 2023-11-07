4a66afdacce9bdce1f5468e8edd9d2f697c48306
wilstermanz <wilstermanz@gmail.com>
2023-11-07 15:04:48 -0600

    Solution for task 3

---
+++ 4-createdir.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Task 3"""
+from snakebite.client import Client
+
+def createdir(l):
+    """Creates directories listed in l"""
+    client = Client("localhost", 9900)
+    for x in client.mkdir(l):
+        print(x)
+
+
+
+if __name__ == "__main__":
+    l = ["/Betty", "/Betty/Holberton"]
+    createdir(l)
