117f040f956463a00d77a92585c091512e88c2b7
wilstermanz <wilstermanz@gmail.com>
2023-11-07 15:32:37 -0600

    Solution for task 5

---
+++ hadoop_project/6-download.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Task 5"""
+from snakebite.client import Client
+
+
+def download(l):
+    """
+    Retrieves from the HDFS files listed in `l`
+    and stores them in the home /tmp folder of the user
+    """
+    client = Client("localhost", 9900)
+    dest = '/tmp/'
+    for x in client.copyToLocal(l, dest):
+        print(x)
+
+
+if __name__ == "__main__":
+    l = ["/holbies/input/lao.txt"]
+    download(l)
+    lao =  open("/tmp/lao.txt", "r")
+    print(lao.read())
+    lao.close()
